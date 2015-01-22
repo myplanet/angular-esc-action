5b6f3828229bdf8b24a98b1a8dafbfa3bdc369a8
Ates Goral <ates.g@myplanetdigital.com>
2015-01-22 14:57:46 -0500

    Added test and Travis config

--- test/escActionSpec.js
+++ test/escActionSpec.js
@@ -1,5 +1,25 @@
 describe('escAction', function () {
-	it('does magic', function () {
+    var actionStub,
+        dom;
 
-	});
+    beforeEach(module('mp.escAction'));
+
+    beforeEach(inject(function ($rootScope, $compile) {
+
+        actionStub = $rootScope.actionStub = jasmine.createSpy('actionStub');
+
+        dom = angular.element(
+            '<div esc-action="actionStub()">' +
+            '</div>'
+        );
+
+        $compile(dom)($rootScope);
+        $rootScope.$digest();
+    }));
+
+    it('evaluates the action expression when the ESC key is pressed', function () {
+        dom.triggerHandler({ type: 'keyup', which: 27 });
+
+        expect(actionStub).toHaveBeenCalled();
+    });
 });
